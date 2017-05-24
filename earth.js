a73cb70feb01286e0010e529e6b6649c5dc76a98
Jonas Almeida <jalmeida@mathbiol.org>
2017-05-24 17:24:11 -0400

    interactive map next to static

--- earth.js
+++ earth.js
@@ -62,10 +62,15 @@ if(earth.div){
         let h = '<button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-home"></span></button>'
         h+=' Latitude:<input id="latitudePos" value="40" size=5 style="color:blue;text-align:right">;  Longitude:<input id="longitudePos" value="-73" size=5 style="color:blue;text-align:right">'
         h+='<br>'
+        h+='<div class="row">'
+           h+='<div class="col-md-6"><div id="imMap" style="width:640px;height:640px"></div></div>'
+           h+='<div class="col-md-6"><img id="imgImg" with="640px" height="640px"></div>'
+        h+='</div>'
         earth.imgDiv.innerHTML= h
-        earth.im = new Image
-        $(earth.im).appendTo(earth.imgDiv)
+        //earth.im = new Image
+        //$(earth.im).appendTo(earth.imgDiv)
         //earth.im.src="https://maps.googleapis.com/maps/api/staticmap?size=1000x1000&maptype=satellite&key="+apiKey.value+"&visible=29.8,-13.09&visible=27.38,-18.53"
+        earth.im=document.getElementById('imgImg')
         earth.im.src="https://maps.googleapis.com/maps/api/staticmap?size=1000x1000&maptype=satellite&key="+apiKey.value+"&visible=41,-73&visible=40.5,-73.5"
         earth.im.onload=function(){
             localStorage.imgkey=apiKey.value
@@ -74,17 +79,18 @@ if(earth.div){
         earth.im.onerror=function(){
             earthMsg.innerHTML='<span style="color:red">error loading image, maybe invalid key? bad connection?</span>'
         }
-        earth.imMap=document.createElement('div')
-        earth.imMap.id='imMap'
-        $(earth.imMap).appendTo(earth.imgDiv)
+        earth.imMap=document.getElementById('imMap')
         earth.imMapFun=function () {
-        // Create a map object and specify the DOM element for display.
-        var map = new google.maps.Map(earth.imMap, {
-          center: {lat: -34.397, lng: 150.644},
-          scrollwheel: false,
-          zoom: 8
-        });
-      }
+            // Create a map object and specify the DOM element for display.
+            var map = new google.maps.Map(earth.imMap, {
+                center: {lat: -34.397, lng: 150.644},
+                scrollwheel: false,
+                zoom: 8
+            });
+        }
+        earth.imMapFun()
+        
+
     }
 
     
